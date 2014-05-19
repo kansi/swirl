538b7c11f8fc2edd2a550b4a85cb52e0b7fba91b
kansi <kansi13@gmail.com>
2014-05-19 12:33:13 +0530

    added build_tree function

--- feature/mtree.erl
+++ feature/mtree.erl
@@ -1,26 +1,31 @@
 %% -*- tab-width: 4;erlang-indent-level: 4;indent-tabs-mode: nil -*-
 %% ex: ft=erlang ts=4 sw=4 et
 %% Licensed under the Apache License, Version 2.0 (the "License"); you may not
-%% use this file except in compliance with the License. You may obtain a copy of
-%% the License at
+%% use this file except in compliance with the License. You may obtain a copy
+%% of the License at
 %%
 %% http://www.apache.org/licenses/LICENSE-2.0
 %%
 %% Unless required by applicable law or agreed to in writing, software
 %% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 %% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-%% License for the specific language governing permissions and limitations under
-%% the License.
+%% License for the specific language governing permissions and limitations
+%% under the License.
+
+%% TODO (thoughts) -> use ETS table to store the merkle tree and use segment
+%% tree structure the process the queries quickly. Ensure that the tree is
+%% padded and has 2^n number of leaf nodes.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% @doc Library module for implementing merkle tree API functions.
+%% @doc This library module implements merkle tree API functions.
 %% <p> API for merkle hash tree </p>
 %% @end
--module(merkle).
+-module(mtree).
 
 -export([new/1,
          insert/2,
          remove/2,
+         build_tree/1,
          root_hash/1,
          get_peak_hash/1,
          get_hash_by_index/2,
@@ -40,17 +45,27 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc Initialize a new merkle hash tree.
 %% @end
--spec new( crypto:hash_algorithms() ) -> mtree().
+-spec new(crypto:hash_algorithms()) -> mtree().
 new(_Sha) -> ok.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% @doc insert/2 add new chunk hash into the tree
+%% @doc Takes input a list of indexes or bin numbers and constructs a tree
+%% using the leaf nodes sotred in the ETS table and store the tree back into
+%% the ETS table.
+%% @end
+-spec build_tree(list()) -> mtree().
+build_tree(_List) -> ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc insert/2 add new chunk hash, bin number of the hash, starting bin
+%% number of the range represented by this bin number and lenght of the range
+%% <p> Provides interface to insert hashs into the ETS table. </p>
 %% @end
 -spec insert(mtree(), binary()) -> mtree().
 insert(_Tree, _Leaf) -> ok.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% @doc remove/2 removes a given hash from tree
+%% @doc remove/2 removes a given hash from tree and re-calculate the root hash
 %% @end
 -spec remove(mtree(), hash()) -> mtree().
 remove(_Tree, _Hash) -> ok.
@@ -64,9 +79,10 @@ root_hash(_Tree) -> ok.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc get_peak_hash/1 returns the peak hashes of the given tree.
 %% <p> Provides an interface to get the peak hashes that are use by the reciver
-%% for reliable file size detection and download/live streaming unification </p>
+%% for reliable file size detection and download/live streaming unification
+%% </p>
 %% @end
--spec get_peak_hash(mtree()) -> hash_list(). %%list of hashes
+-spec get_peak_hash(mtree()) -> hash_list().
 get_peak_hash(_Tree) -> ok.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -89,7 +105,8 @@ get_hash_by_index(_Tree, _Bin_Number) -> ok.
 %% get a transient root hash or munro hash of this newly generated subtree. Now
 %% when the peer requests a chunk of this new subtree, the munro hash is sent
 %% along with the necessary uncle hashes which will be used to calculate the
-%% munro hash of the received chunk and compare it to the received munro hash.</p>
+%% munro hash of the received chunk and compare it to the received munro
+%% hash.</p>
 %% @end
 -spec get_munro_hash(mtree(), [integer()]) -> hash().
 get_munro_hash(_Tree, _Bin_Numbers) -> ok.
@@ -97,8 +114,8 @@ get_munro_hash(_Tree, _Bin_Numbers) -> ok.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc verify_munro_hash/2 returns true if the calculated Munro_Hash from the
 %% Uncle_Hashes is equal to the recieved Munro_Hash.
-%% <p> Uses verify function internally which is provided the new subtree as Tree
-%% and Munro_Hash as Root_Hash as its arguments.</p>
+%% <p> Uses verify function internally which is provided the new subtree as
+%% Tree and Munro_Hash as Root_Hash as its arguments.</p>
 %% @end
 -spec verify_munro_hash(mtree(), hash(), hash_list()) -> {true, mtree()}
                                                        | {false, mtree()}.
@@ -106,27 +123,38 @@ verify_munro_hash(_Tree, _Munro_Hash, _Uncle_Hashes) -> ok.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% TODO : verify if it matches the specs.
-%% @doc verify/3 inserts the Uncle_Hashes into the tree and returns true
-%% if the new Root_Hash of the tree after addition of a new chunk is same as the
+%% @doc verify/3 inserts the Uncle_Hashes into the tree and returns true if the
+%% new Root_Hash of the tree after addition of a new chunk is same as the
 %% received Root_Hash.
-%% <p> When a peer receives a new chunk it MUST recieve Uncle_Hashes required to
-%% verify to calculate the Root_Hash and check it against the received Root_Hash
-%% </p>
+%% <p> When a peer receives a new chunk it MUST recieve Uncle_Hashes required
+%% to verify to calculate the Root_Hash and check it against the received
+%% Root_Hash </p>
 %% @end
 -spec verify(mtree(), hash_list(), hash()) -> {true, mtree()}
-                                            | {false, mtree()}.
+                                            | {false,mtree()}.
 verify(_Tree, _Uncle_Hashes, _Root_Hash) -> ok.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc dump_tree/2 write the merkle hash tree into a file with name as
-%% File_Name.
+%% File_Name. Reutrns ok if the operation succeded.
 %% @end
 -spec dump_tree(mtree(), string()) -> ok | error.
 dump_tree(_Tree, _File_Name) -> ok.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% @doc load_tree/1 loads merkle hash tree from a file with name File_Name.
+%% @doc load_tree/1 loads merkle hash tree from a file with name as File_Name.
 %% @end
--spec load_tree(string()) -> ok
-                           | error.
+-spec load_tree(string()) -> mtree().
 load_tree(_File_Name) -> ok.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% INTERNAL FUNCTIONS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc
+%% @end
+%-spec padding(integer()) -> integer() | ok.
+%padding(_Tree) -> ok.
+
